f896c94e61921028ed8a01c51a8efe4dc6177669
heyitsmartin <m274li@uwaterloo.ca>
2015-03-29 03:31:04 -0400

    Loading and cleanup
    Added loading messages and a option to clear highlighted words

--- find.js
+++ find.js
@@ -1,63 +1,98 @@
-chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
-    
+
+
+
+chrome.runtime.onMessage.addListener(function(message,sender,respone){
     jQuery.fn.highlight=function(c){function e(b,c){var d=0;if(3==b.nodeType){var       a=b.data.toUpperCase().indexOf(c),a=a-(b.data.substr(0,a).toUpperCase().length-b.data.substr(0,a).length);if(0<=a){d=document.createElement("span");d.className="highlight";a=b.splitText(a);a.splitText(c.length);var f=a.cloneNode(!0);d.appendChild(f);a.parentNode.replaceChild(d,a);d=1}}else if(1==b.nodeType&&b.childNodes&&!/(script|style)/i.test(b.tagName))for(a=0;a<b.childNodes.length;++a)a+=e(b.childNodes[a],c);return d} return this.length&&c&&c.length?this.each(function(){e(this,c.toUpperCase())}):this};jQuery.fn.removeHighlight=function(){return this.find("span.highlight").each(function(){this.parentNode.firstChild.nodeName;with(this.parentNode)replaceChild(this.firstChild,this),normalize()}).end()};
+    function find ( method) {
+        window.numberOfWords = 20; 
+        var numberOfWords = window.numberOfWords;
+        var foundin = new  Array(); 
+        // message.searchText is the text that was captured in the popup    
+        // Search/Highlight code goes here
+        //alert("Swag to the max" + message.searchString[0]);
+        var ran = window.check_var;
+        if( document.readyState == "complete" && ran == null ) {
+            window.check_var = false;
 
-    // message.searchText is the text that was captured in the popup    
-    // Search/Highlight code goes here
-    //alert("Swag to the max" + message.searchString[0]);
-    var ran = window.check_var;
-    if( document.readyState == "complete" && ran == null ) {
-        window.check_var = false;
-        
-        console.log(message.searchString);   
-        var iM = message.searchString.length;
+            console.log(message.searchString);   
+            var iM = message.searchString.length;
 
 
-        var text = message.searchString;
-        //clean up search strings 
-        for( var tLength = 0 ; tLength < text.length;tLength++) { 
-            text[tLength] =text[tLength].trim();
-        }
+            var text = message.searchString;
+            //clean up search strings 
+            for( var tLength = 0 ; tLength < text.length;tLength++) { 
+                text[tLength] =text[tLength].trim();
+            }
 
-        console.log(text);
-        //get html code
-       
-       
+            console.log(text);
+            //get html code
 
-        try{
-            e = document.body.innerHTML;
-            console.log("Found innerHTML");
-            console.log(e);
-        }catch( ex ) {
-            console.log("An error happened innerHtml could not be found  "+ ex);
-        }
-        if( e != "" ) {
-            //console.log(e);
 
-            //clear old highlights
-//            var enew = e.replace(/(<mark>|<\/mark>)/igm, "");
-//            document.body.innerHTML = enew;
-            var newe = e ; 
-            for (var i = 0 ; i< text.length; i++ ) {
-                //Don't search for term >1 word
-                if(text[i].replace(" ", "").length - text[i].length != 0 ) { 
-                    continue;
+
+            try{
+                e = document.body.innerHTML;
+                console.log("Found innerHTML");
+
+            }catch( ex ) {
+                console.log("An error happened innerHtml could not be found  "+ ex);
+            }
+            if( e != "" ) {
+                var newe = e ; 
+                window.text=text;
+                if( numberOfWords >= text.length) { 
+                    numberOfWords = text.length; 
                 }
+                //search for the original word: 
+
+                for (var i = 0 ; i< numberOfWords; i++ ) {
+                    //Don't search for term >1 word
+                    if(text[i].replace(" ", "").length - text[i].length != 0 ) { 
+                        continue;
+                    }
                     console.log("Highlighting: "+text[i]);
                     $('div').highlight(text[i]);
-//                var query = new RegExp("(\\b" + text[i] + "\\b)", "gim");
-//                var newe = newe.replace(query, "<mark>"+text[i]+"</mark>");
+                    //foundin= foundin.concat($('*:contains('+text[i]+')'));
+                }
+
             }
-//            console.log("setting new string");
-//            document.body.innerHTML = newe;
+
+
+        }
+        console.log("Done.");
+
+
+        //Tell popup page when search is done 
+        //     chrome.tabs.query({active:true,currentWindow:true}, function( tabs) { 
+        //     chrome.tabs.executeScript(tabs[0].id, {file:"find.js"});
+        //     chrome.tabs.sendMessage(tabs[0].id,{method:'find', searchString:sWords});
+        //    
+        respone(foundin);
+        return;
+
+    }
+    function prev (message) { 
+        if(message.div!= null) {
+            var focusDiv = message.div; 
+            console.log(focusDiv.getText);
+            //focusDiv.focus();
+        }else { 
+            console.log(message);
         }
 
 
     }
-    console.log("Done.");
-    
+    function clear() { 
+        console.log('removing highlights...');
+        $('div').removeHighlight();
+    }
+    if(message.method =='find'){ 
+        find(message);
+    }else if (message.method == 'prev'){
+        prev(message);
+    }else if(message.method =='clear'){ 
+        clear();
+    }
 
-    
-    return;
 
 });
+
--- popup.js
+++ popup.js
@@ -1,8 +1,11 @@
     
 
-var SearchText = ""; 
-
+var SearchText = ''; 
+var words=null;
+var counter =0 ; 
 var request = null;
+
+
 function makeHttpObject() {
   try {return new XMLHttpRequest();}
   catch (error) {alert("XMLHTTPREQUESTFAILED");}
@@ -15,22 +18,34 @@ function makeHttpObject() {
 }
 
 
-function grabSyns(sF) { 
+function grabSyns(sF) {
+    //loading message 
+//    var img = new Image();
+    var div = document.getElementById("loadDiv");
+//    img.src = './load.gif';
+//    img.style.height='30px';
+//    img.style.width='30px';
+//    div.appendChild(img)
+    div.textContent= 'fetching words..';
+    
+    
+    
    SearchText = document.getElementById('searchString').value;
     
     if( SearchText != "" ) { 
         
         request = makeHttpObject(); 
-        //pls no ddos 
+        //pls no ddos >_> 
         var url = "http://heyitsmartin.com/uniSearch/return.php?word="+SearchText;
         //var url = "http://google.com";
         request.onerror = function() { 
-                alert("you fucked up");
+                alert("Something died");
             }
         request.onreadystatechange=function(){
             
             if(request.readyState==4 && request.status==200){ 
                 console.log(request.responseText);
+                div.textContent= 'highlighting...';
                 sF(request.responseText);
             }
         }
@@ -47,35 +62,113 @@ function grabSyns(sF) {
     
 }
 
+function setDivs(divs) { 
+    words = divs; 
+    document.getElementById('loadDiv').textContent='';
+}
+
+//Send information to injected script 
 function startFind(returnString) { 
     if(returnString != "") { 
         var sWords = returnString.split(","); 
-        chrome.tabs.query({active:true,currentWindow:true}, function( tabs) { 
-            chrome.tabs.executeScript(tabs[0].id, {file:"find.js"});
-            chrome.tabs.sendMessage(tabs[0].id,{method:'find', searchString:sWords});
+        chrome.tabs.query({
+            active:true,
+            currentWindow:true
+        }, function( tabs) { 
+            chrome.tabs.executeScript(
+                tabs[0].id, {
+                file:"find.js"});
+            
+            chrome.tabs.sendMessage(tabs[0].id,
+                                        {method:'find',
+                                         searchString:sWords},
+                                    setDivs);
         });
     }
-    
-    
 }
 
 function submitSearch() { 
     var returnString = ""; 
     returnString = grabSyns(startFind); 
+}
+
+function focusObj ( methodName,  object )  { 
+  
+        chrome.tabs.query({
+            active:true,
+            currentWindow:true
+        }, function( tabs) { 
+            chrome.tabs.executeScript(
+                tabs[0].id, {
+                file:"find.js"});
+            
+            chrome.tabs.sendMessage(tabs[0].id,
+                                        {method:methodName,
+                                         div:object});
+        });
+
+}
+
+function goPrev() { 
+    if(words==null) return;
+    if(counter> 0 ) { 
+        counter -- ; 
+    }else { 
+        counter = words.length-1 ;
+    }
+    focusObj('prev',words[0]);
     
     
-    
+
+}
+function goNext() { 
+    if(words==null) return;
+    if(counter > words.length-1){
+        counter++ ;
+    }else{ 
+        counter =0       ;
+    }
+     focusObj('next ',words[0]);
+
+
+}
+
+function clear(){ 
+     chrome.tabs.query({
+            active:true,
+            currentWindow:true
+        }, function( tabs) { 
+            chrome.tabs.executeScript(
+                tabs[0].id, {
+                file:"find.js"});
+            
+            chrome.tabs.sendMessage(tabs[0].id,
+                                        {method:'clear'});
+        });
+
 }
 
 window.onload = function(){     
     document.getElementById("searchString").focus();
     //document.getElementById('searchButton').onclic = searchText;
 
-   // alert(searchTest);
+    //Listner for search button 
     var SearchButton = document.getElementById('searchButton'); 
     SearchButton.addEventListener("click", submitSearch );
     
+    //Listner for clear button 
+    var SearchButton = document.getElementById('clear'); 
+    SearchButton.addEventListener("click", clear );
     
+    //Listner for prev arrow 
+    //var SearchButton = document.getElementById('prev'); 
+    //SearchButton.addEventListener("click", goPrev );
+    
+    //Listner for next arrow
+   // var SearchButton = document.getElementById('next'); 
+   // SearchButton.addEventListener("click", goNext );
+    
+    //Listener for textbox 
     document.getElementById("searchString").addEventListener("keydown",function(key) {
         if(key.keyCode == 13 ) { 
             submitSearch();
